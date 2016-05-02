9131226d8dbca976a3ecf361b20ccfbbd4f19bb1
treeplantedbystreams <davbros88@hotmail.com>
2016-05-02 12:49:34 -0500

    ok

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -1,9 +1,11 @@
 var app = angular.module("myBlogApp.controllers",[])
-app.controller("listController",['$scope',"$http", function($scope, $http){
+app.controller("listController",['$scope',"$http" ,function($scope, $http){
   $scope.posts= [];
   $http.get("http://localhost:3000/api/posts/").success(function(data){
       $scope.posts = data;
   });  
+
+
 
 }]);
 app.controller("submitPostController",['$scope',"$http","postFactory", function($scope, $http, postFactory){
